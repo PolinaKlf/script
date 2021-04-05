58c58791a3a347bd60d21357af33763c5ab29470
PolinaKlf <polina.klyutko@mail.ru>
2021-04-05 18:45:36 +0300

    mainscript

---
+++ mainscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir mainscript
+cd /c/Users/Полина/Desktop/script/script/mainscript
+> newfile.txt
+> newfile1.txt
+> newfile2.txt
+echo "Hello it's me" > newfile.txt
+echo "Bye Bye" > newfile1.txt
+echo "wait" > newfile2.txt
+cat newfile.txt newfile1.txt newfile2.txt
