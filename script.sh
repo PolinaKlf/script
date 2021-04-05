deaf9a315622fd8652d17be37baefcbd31d1ce16
PolinaKlf <polina.klyutko@mail.ru>
2021-04-05 11:36:42 +0300

    commit1

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello it's me"
+echo "let's get started"
+ls
+ls -l
+echo "Done"
---
+++ script2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mycomputer="Lenovo"
+myOS=`uname-a`
+
+echo "this script name is $0"
+echo "Hello $1"
+echo "Hello $2"
+
+num1=50
+num2=45
+summa=$((num1+num2))
+echo "$num1 + $num2 = $summa"
+
+ 
